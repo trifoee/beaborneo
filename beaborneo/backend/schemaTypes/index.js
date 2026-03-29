11904c57af6412876da50a48a4c70b2406864017
cjasperp <cornileusjasper@gmail.com>
2026-03-29 17:46:06 +0800

    feat: added activity package and tour

--- beaborneo/backend/schemaTypes/index.js
+++ beaborneo/backend/schemaTypes/index.js
@@ -5,5 +5,6 @@ import our_story from './aboutUs/our_story'
 import our_value from './aboutUs/our_value'
 import transport_service from './transport_service/transport_service'
 import social_links from './social_links/social_links'
+import activity_package from './tours/activity_package'
 
-export const schemaTypes = [ tour ,contact_information , galleryImage , our_story , our_value, transport_service ,social_links ]
+export const schemaTypes = [ tour , activity_package , contact_information , galleryImage , our_story , our_value, transport_service ,social_links ]
